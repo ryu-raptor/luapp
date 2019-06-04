ca922b6b619fb891fd8e3f888deec2cc00e5ee32
ryu-raptor <ryu_omnibook@icloud.com>
2019-06-04 19:55:14 +0900

    Initial commit

---
+++ genluapp.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "$0 [appdir] [entry script name]"
+    exit
+fi
+
+# change directory
+cd $1
+
+# correct info
+scriptName=`basename $2 .lua`
+
+# check file existence.
+if ! [ -e "${scriptName}.lua" ]; then
+    echo "${scriptName}.lua doesn't exist."
+    exit
+fi
+
+if [ -e app.sh ]; then
+    echo "app.sh file exists! Move the file. Abort"
+    exit
+fi
+
+# build app.sh
+cat <<EOF > app.sh
+#!/bin/bash
+appdir=\`dirname \$0\`
+LUA_PATH="\${LUA_PATH};\${appdir}/?.lua"
+export LUA_PATH
+lua \${appdir}/${scriptName}.lua "\$@"
+EOF
+
+# give execution permission
+chmod u+x app.sh
+
+# then make symlink to app.sh with name of appdir
+appName=`basename $(pwd)`
+target=`readlink -f app.sh`
+ln -s $target $appName
+
